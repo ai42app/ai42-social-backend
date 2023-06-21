8962e6dcb83469f091cc10977fac4cc4ddd97c60
satnamsingh611 <125273094+satnamsingh611@users.noreply.github.com>
2023-06-21 17:01:45 +0530

    create_image_table

---
+++ userController/openJourney.js
@@ -0,0 +1,110 @@
+const connection=require('../database/database');
+var tableName = 'user_images';
+
+
+
+const database = () => {
+    try {
+      connection.connect();
+      console.log('Connected to the database!');
+    } catch (error) {
+      console.log('Database connection failed:', error);
+      process.exit(1); // Terminate the application if the database connection fails
+    }
+};
+database();
+
+exports.saveImages=(req,res)=>{
+
+    const { link_to_image,creator,keywords,date,likes } = req.body;
+    
+    
+ 
+    const query = `INSERT INTO ${tableName} (link_to_image,creator,keywords,date,likes) VALUES (?,?,?,?,?)`;
+
+    const values =[link_to_image, creator, keywords, date,likes]
+    
+    connection.query(query, values, (error, results) => {
+      if (error) {
+        console.error("Error during inserting  Image:", error);
+        return res
+          .status(500)
+          .json({ error: "Failed to insert Image", dbError: error.message });
+      }
+  
+      res
+        .status(200)
+        .json({ success: true, message: "Image Saved...",id: results.insertId});
+    });
+   
+}
+
+exports.likeImage=(req,res)=>{
+    const { id } = req.params;
+    
+    const query = `UPDATE ${tableName} SET likes=likes +1     WHERE id=${id}`;
+    const values = [id];
+  
+    connection.query(query, values, (error, results) => {
+      if (error) {
+        console.error("Error updating image like:", error);
+        return res
+          .status(500)
+          .json({ error: "Failed to update image", dbError: error.message });
+      }
+  
+      if (results.affectedRows === 0) {
+        return res.status(404).json({ error: "Image not found" });
+      }
+  
+      console.log("Image updated successfully");
+      res.status(200).json({success: true,message: "Like added successfully"});
+    });
+}
+
+
+exports.getImage=(req,res)=>{
+    const { id } = req.params;
+
+    const query = `SELECT * FROM ${tableName} WHERE id = ${id}`;
+    const values = [id];
+  
+    connection.query(query, values, (error, results) => {
+      if (error) {
+        console.error("Error fetching user:", error);
+        return res
+          .status(500)
+          .json({ error: "Failed to fetch user", dbError: error.message });
+      }
+  
+      if (results.length === 0) {
+        return res.status(404).json([]);
+      }
+  
+        console.log("User fetched successfully");
+        res.status(200).json([results[0]]);
+    });
+}
+
+exports.getImages=(req,res)=>{
+    const { limit } = req.params;
+
+    const query = `SELECT * FROM ${tableName}`;
+    const values = [];
+  
+    connection.query(query, values, (error, results) => {
+      if (error) {
+        console.error("Error fetching user:", error);
+        return res
+          .status(500)
+          .json({ error: "Failed to fetch user", dbError: error.message });
+      }
+  
+      if (results.length === 0) {
+        return res.status(404).json([]);
+      }
+  
+        console.log("User fetched successfully");
+        res.status(200).json(results);
+    });
+}
--- userRoute/userRouter.js
+++ userRoute/userRouter.js
@@ -1,115 +1,35 @@
 const express = require('express');
 const router = express.Router();
-
-const connection=require('../database/database')
-
-
-const database = () => {
-    try {
-      connection.connect();
-      console.log('Connected to the database!');
-    } catch (error) {
-      console.log('Database connection failed:', error);
-      process.exit(1); // Terminate the application if the database connection fails
-    }
-  };
-database()
-
-const tableName = 'userresponse';
+const userController =require('../userController/userController');
+const openJourney =require('../userController/openJourney');
 
 
 
-router.post('/users', (req, res) => {
-    
-  //const { id, text,createdAt,ai} = req.body;
-  console.log(req.body);
-  var data = req.body;
+//create API
 
-  console.log(data);
+router.post('/emailhashuser',userController.useremailhash)
 
-  var sql = `INSERT INTO ${tableName} (id,text, createdAt,ai) VALUES ?`;
-  let Values=[];
+router.post('/createChannel',userController.createChannel)
 
-
-  data.forEach(item => {
-    Values.push([item.id,item.text,item.createdAt,item.ai]);
-  });
-  
-   connection.query(sql,[Values], (error, results) => {
-      if (error) {
-        console.error('Error inserting user:', error);
-        return res.status(500).json({ error: 'Failed to insert user', dbError: error.message });
-      }
-  
-      console.log('User inserted successfully');
-      res.status(200).json({ success: true, message: "User inserted successfully" });
-    });
-  
-});
+router.post('/users',userController.convohistory);
+router.post('/saveImages',openJourney.saveImages);
+router.post('/likeImages/:id',openJourney.likeImage);
 
 
-router.get('/getusers', (req, res) => {
-    
-    connection.query(`SELECT * FROM  ${tableName}`, (error, results) => {
-      if (error) {
-        console.error('Error inserting user:', error);
-        return res.status(500).json({ error: 'Failed to insert user', dbError: error.message });
-      }
-  
-      console.log('User inserted successfully');
-      res.status(200).json({ success: true, message: "User inserted successfully" ,results:results});
-    });
-  });
-  
+
+ /// UPDATE TEXT CANVO
+router.put("/users/:id",userController.updateConvo);
+
+
+
+/// GET APIs
+router.get("/getusers",userController.getConvohistory);
+router.get('/getImages/',openJourney.getImages);
+router.get('/getImage/:id',openJourney.getImage);
+router.get("/users/:id",userController.getcanvoid);
 
   module.exports = router
 
 
 
 
-
-// const express = require('express');
-
-// const tableName = 'userresponse';
-// const router = express.Router();
-
-// const connection=require('../database/database')
-// // Handle the POST request
-// router.post('/api/endpoint', (req, res) => {
-//   const data = req.body;
-
-//   // Connect to MySQL database
-//   const database = () => {
-//     try {
-//       connection.connect();
-//       console.log('Connected to the database!');
-//     } catch (error) {
-//       console.log('Database connection failed:', error);
-//       process.exit(1); // Terminate the application if the database connection fails
-//     }
-//   };
-// database()
-//   // Insert each object into the database
-//   router.post('/post', (req,res)=>{
-//      data.forEach(obj => {
-//        connection.query(`INSERT INTO ${tableName}`, obj, (error, results) => {
-//         if (error) {
-//           // Handle any database errors
-//           console.error(error);
-//         } else {
-//           // Data inserted successfully
-//           console.log('Data inserted:', obj);
-//         }
-//       });
-//     });
-  
-//   // Send a response
-//   res.sendStatus(200);
-//   })
-
-//   // Close the database connection
-//   connection.end();
-
-// });
-
-// module.exports=router
